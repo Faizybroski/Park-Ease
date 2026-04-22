059dcdd418522975483529a145931a2ac027b175
Faizy Broski <faizanhashmi5813@gmail.com>
2026-04-22 22:11:07 +0500

    Fix item as variant in home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -159,7 +159,7 @@ const item = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.4, ease: "easeOut" },
+    transition: { duration: 0.4, ease: "easeOut" as const },
   },
 };
 
